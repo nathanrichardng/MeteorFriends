8a4d24eeed2c9ce1051a74fe6b700cb094af7633
Nathan <nathanrichardng@gmail.com>
2015-11-01 00:20:20 -1000

    updated user.colleagues to user.friends in remove friend method

--- FriendMethods.js
+++ FriendMethods.js
@@ -2,8 +2,8 @@ if (Meteor.isServer) {
   Meteor.methods({
       'removeFriend': function(friendId){
         var userId = Meteor.userId();
-          Meteor.users.update({ _id: userId }, {$pull: {colleagues: friendId} });
-          Meteor.users.update({ _id: friendId }, {$pull: {colleagues: userId} });
+          Meteor.users.update({ _id: userId }, {$pull: {friends: friendId} });
+          Meteor.users.update({ _id: friendId }, {$pull: {friends: userId} });
           FriendRequests.update({ $or: [{ to: userId, from: friendId }, { to:friendId, from: userId }] }, {$set: {status: 'Rejected'} }, { multi: true });
       }
   });  
