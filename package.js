aa91f21ca635a1dd905b330b756e361294ee5b8c
Nathan <nathanrichardng@gmail.com>
2015-11-01 00:09:41 -1000

    updated version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'sodiumtellurium:friends',
-  version: '0.0.6',
+  version: '0.0.7',
   // Brief, one-line summary of the package.
   summary: 'A basic friends package',
   // URL to the Git repository containing the source code for this package.
